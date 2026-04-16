2ac477cc819c9641195dba604721ac5618da5729
mehadishisir <mehadishisir@gmail.com>
2026-04-16 11:35:40 +0600

    add tailwind css and lato font

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,12 @@
+<link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet"></link>
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),tailwindcss()],
 })
